59076cb29887d4548f6d799ac916a521f321f760
Francisco Zorat <franciscozorat@gmail.com>
2024-01-20 03:05:58 -0300

    feat: actual test

--- test.ts
+++ test.ts
@@ -1,22 +1,32 @@
+import fs from 'node:fs';
 import { Canvas } from 'canvas';
 import { drawText } from './index';
 
-const canvas = new Canvas(512, 512);
+const canvas = new Canvas(500, 500);
 const ctx = canvas.getContext('2d');
-const text = 'The old rusted farm equipment surrounded the house predicting its demise. He uses onomatopoeia as a weapon of mental destruction.';
+const text = 'The old rusted farm equipment 🤪 surrounded the house predicting its demise. He uses onomatopoeia as a weapon of mental destruction. 👍';
 const options = {
     verbose: true,
     rect: {
-        x: 25,
-        y: 25,
-        width: canvas.width - 25,
-        height: canvas.height - 25
+        x: 50,
+        y: 50,
+        width: canvas.width - 50,
+        height: canvas.height - 50,
     },
 };
 
 const test = async () => {
+    /* --- background --- */
+    ctx.fillStyle = '#4a4aad';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    /* --- font color --- */
+    ctx.fillStyle = '#ceceec';
+
     const fontSizeUsed = await drawText(ctx, text, options);
-    console.log('Font size used: ', fontSizeUsed)
+    console.log('Font size used: ', fontSizeUsed);
+
+    const buffer = canvas.toBuffer();
+    fs.writeFileSync('test.png', buffer);
 };
 
 test();
